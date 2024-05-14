c68c9d58e24482b9832363c88f335b7661492a3b
Amy Rixon <rixonamy@gmail.com>
2024-05-14 15:31:17 +0000

    Add multiplication game code

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,7 +31,10 @@ function runGame(gameType) {
 
     if (gameType === "addition"){
         displayAdditionQuestion(num1, num2);
-    } else {
+    } else if (gameType=== "multiply") {
+        displayMultiplyQuestion (num1, num2);
+    }
+    else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
     }
@@ -70,6 +73,8 @@ function calculateCorrectAnswer(){
 
     if (operator === "+"){
         return [operand1 + operand2, "addition"];
+    } else if (operator === "x") {
+        return [operand1 * operand2, "multiply"];
     } else {
         alert(`Unimplemented opearatore ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
@@ -109,8 +114,10 @@ function displaySubtractQuesiton(){
 
 }
 
-function displayMultiplyQuestion(){
-
+function displayMultiplyQuestion(operand1, operand2){
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "x";
 }
 
 function displayDivisionQuestion(){
